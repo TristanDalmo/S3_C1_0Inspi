6106076f8a4a3c42eae2b9dff86bd2d4592b8608
julesDutrion <jules.dutrion@iut-dijon.u-bourgogne.fr>
2024-10-01 15:23:59 +0200

    Modification de l'accueil.js pour coder le lancement de la page formulaire

--- PHP/CreationPages/GenerationFormulaire.php
+++ PHP/CreationPages/GenerationFormulaire.php
@@ -2,12 +2,20 @@
     require_once("GenerationPage.php");
     
     /**
-     * Classe de génération de la page d'accueil de l'application
+     * Classe de génération de la page de la création des formulaires de l'application
      */
     class GenerationAccueil extends GenerationFormulaire
     {
-
-
+        public function __construct()
+        {
+            // On appelle le constructeur de la classe abstraite, définissant les chemins vers le script et le style
+            parent::__construct(cssChemin: "../SiteWeb/menu.css",jsChemin: "../metier/Accueil.js");
+        }
+        
+        protected GenerateContent() : string
+        {
+            echo "bienvenue sur le formulaire";
+        }
     }
 
 ?>
