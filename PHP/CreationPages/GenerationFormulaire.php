ff48160bbb9a2cfd47d92bfd7b88563b174d2ee3
julesDutrion <mysterio.astroman@gmail.com>
2024-10-01 14:44:40 +0200

    Commencement de l'ajout de la création de la page formulaire

--- PHP/CreationPages/GenerationFormulaire.php
+++ PHP/CreationPages/GenerationFormulaire.php
@@ -1,6 +1,13 @@
 <?php
+    require_once("GenerationPage.php");
     
+    /**
+     * Classe de génération de la page d'accueil de l'application
+     */
+    class GenerationAccueil extends GenerationFormulaire
+    {
 
 
+    }
 
 ?>
