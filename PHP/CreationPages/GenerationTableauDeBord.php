a121ea39d6907a2c7e96020edafd4db731c63854
julesDutrion <jules.dutrion@iut-dijon.u-bourgogne.fr>
2024-10-03 11:44:41 +0200

    Correction des bugs lié au changement de page dans l'acceuil

--- PHP/CreationPages/GenerationTableauDeBord.php
+++ PHP/CreationPages/GenerationTableauDeBord.php
@@ -10,7 +10,7 @@ class GenerationTableauDeBord extends AbstractGenerationPage
     public function __construct()
     {
         // On appelle le constructeur de la classe abstraite, définissant les chemins vers le script et le style
-        parent::__construct(cssChemin: "../SiteWeb/formulaire.css");
+        parent::__construct(cssChemin: "../SiteWeb/formulaire.css",nom:"E-Lieu ~ Tableau de bord");
     }
 
     protected function GenerateContent() : string
