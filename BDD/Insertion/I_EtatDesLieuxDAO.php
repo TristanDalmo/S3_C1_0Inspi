93a6cd448b6e3633e31d6fd6cdfea13af6306636
MatteoDeMarcoD1 <matteo.de-marco@iut-dijon.u-bourgogne.fr>
2024-10-03 09:41:31 +0200

    ajout de la bdd, connexion, insertion Etat des lieux fermeture connexion et test

---
+++ BDD/Insertion/I_EtatDesLieuxDAO.php
@@ -0,0 +1,29 @@
+<?php
+
+interface I_EtatDesLieuxDAO {
+    /**
+     * Initialise la connexion à la base de données.
+     */
+    public static function init();
+
+    /**
+     * Insère un nouvel état des lieux dans la table.
+     * 
+     * @param EtatDesLieux $etatDesLieux L'objet EtatDesLieux à insérer
+     */
+    public static function insertTable($etatDesLieux);
+
+    /**
+     * Affiche le contenu d'une table spécifiée.
+     * 
+     * @param string $tableName Le nom de la table à afficher
+     */
+    public static function afficherContenuTable($tableName);
+
+    /**
+     * Ferme la connexion à la base de données.
+     */
+    public static function closeConnection();
+}
+
+?>
