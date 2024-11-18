28a33b55a77012d822bdf849b6977c4692b779f2
TristanDalmo <tristan.dal-molin@iut-dijon.u-bourgogne.fr>
2024-11-18 13:45:06 +0100

    Interfaces

---
+++ DAO/Piece/I_ElementsDAO.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DAO\Piece;
+require_once(__DIR__ . "/../../Model/Elements.php");
+use Model\Elements;
+
+/**
+ * Interface d'une DAO d'un élément 
+ */
+interface I_ElementsDAO {
+
+    /**
+     * Méthode permettant de créer un élément dans la BDD
+     * @param \Model\Elements $element Élément à insérer
+     * @return void
+     */
+    public function Create(Elements $element);
+
+    /**
+     * Méthode permettant de modifier les données d'un élément
+     * @param \Model\Elements $element Élément à modifier
+     * @return void
+     */
+    public function Update(Elements $element);
+
+    /**
+     * Méthode permettant de supprimer un élément
+     * @param int $id Id d'un élément à supprimer
+     * @return void
+     */
+    public function Delete(int $id);
+
+    /**
+     * Méthode permettant de sélectionner un élément dans la BDD à partir de son id
+     * @param int $id Id de l'élément à sélectionner
+     * @return \Model\Elements Élément sélectionné
+     */
+    public function getById(int $id) : Elements;
+}
+
+
+
+
+
+?>
