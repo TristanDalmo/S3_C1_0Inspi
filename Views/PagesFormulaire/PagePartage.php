708749dfab43d5345f59db275c2e30ace3b5ec86
Wassim <moulay-wassim.alaoui@iut-dijon.u-bourgogne.fr>
2024-11-22 09:36:56 +0100

    Importation bibliothèque Gotenberg Modifications PagePartage

--- Views/PagesFormulaire/PagePartage.php
+++ Views/PagesFormulaire/PagePartage.php
@@ -4,8 +4,6 @@ namespace Views\PagesFormulaire;
 require_once(__DIR__ . "/../AbstractPage.php");
 use Views\AbstractPage;
 
-
-
 class PagePartage extends AbstractPage
 {
     public function __construct()
@@ -13,19 +11,31 @@ class PagePartage extends AbstractPage
         parent::__construct(cssChemin: "../../Public/CSS/Partage.css", nom: "E-Lieu ~ Partage et Export");
     }
 
-    protected function GenerateContent(): string
+    protected function GenerateContent() : string
     {
         return "
-        <div class='container'>
-            <div class='pdf-container'>
-                <iframe src='../../Public/Medias/TD5.pdf' width='100%' height='500px' title=\"Affichage du PDF Cas d'Utilisation\" referrerpolicy=\"strict-origin-when-cross-origin\"></iframe>
+            <div class='container'>
+                <div class='pdf-container'>
+                    <embed id='pdf-embed' src='../../Public/Medias/TP1ANALYSE.pdf?nocache=" . uniqid() . "' width='100%' height='500px' type='application/pdf'/> 
+                </div>
+                <div class='buttons-container'>
+                    <button onclick='action1()'>Imprimer l'état des lieux</button>
+                    <button onclick='action2()'>Partager par mail</button>
+                    <button onclick='action3()'>Télécharger au format docx</button>
+                    <button onclick='action4()'>Télécharger au format PDF</button>
+                </div>
             </div>
-            <div class='buttons-container'>
-                <form method='POST' action=''>
-                    <button type='submit' name='generate_pdf'>Télécharger au format PDF</button>
-                </form>
-            </div>
-        </div>
+
+            <script>
+                const currentUrl = window.location.href;
+
+                if (!sessionStorage.getItem('redirected')) {
+                    sessionStorage.setItem('redirected', 'true'); // Marque la redirection comme effectuée
+                    window.location.href = currentUrl; // Redirige l'utilisateur vers l'URL exacte
+                } else {
+                    sessionStorage.removeItem('redirected'); // Réinitialise le marqueur pour les visites futures
+                }
+            </script>
         ";
     }
 }
